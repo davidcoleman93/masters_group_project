ae0a2fa47902141328ecf39790c414dd5ad2d1ac
D H <D H>
2017-02-20 20:51:26 +0000

    REST Controller refactored.

--- src/main/java/controller/RESTController.java
+++ src/main/java/controller/RESTController.java
@@ -1,11 +1,13 @@
 package controller;
 
-import business.TestBusinessLocal;
+import business.*;
+import entities.*;
 
 import javax.ejb.EJB;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by C06590861 on 15/02/2017.
@@ -15,18 +17,50 @@ import java.util.Collection;
 public class RESTController {
 
     @EJB
-    private TestBusinessLocal ejb;
+    private EventCauseBusinessLocal eventCauseEJB;
+    @EJB
+    private FailureClassBusinessLocal failureClassEJB;
+    @EJB
+    private FailureEventBusinessLocal failEventEJB;
+    @EJB
+    private MarketOperatorBusinessLocal marketOpEJB;
+    @EJB
+    private UserEventTypeBusinessLocal userEventEJB;
+
 
     @GET
+    @Path("/fail_events")
     @Produces(MediaType.APPLICATION_JSON)
-    public Collection<?> getAll() {
-        return ejb.getAll();
+    public Collection<FailureEvent> getAllFailureEvents() {
+        return (List<FailureEvent>)failEventEJB.getAllFailureEvents();
     }
 
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void addRecord(Object o){
-        ejb.addRecord(o);
+    @GET
+    @Path("/event_causes")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<EventCause> getAllEventCauses(){
+        return (List<EventCause>)eventCauseEJB.getAllEventCauses();
+    }
+
+    @GET
+    @Path("/failure_classes")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<FailureClass> getAllFailureClasses(){
+        return (List<FailureClass>)failureClassEJB.getAllFailureClasses();
+    }
+
+    @GET
+    @Path("/market_ops")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<MarketOperator> getAllMarketOperators(){
+        return (List<MarketOperator>)marketOpEJB.getAllMarketOperators();
+    }
+
+    @GET
+    @Path("/user_events")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<UserEventType> getAllUserEventTypes(){
+        return (List<UserEventType>)userEventEJB.getAllUserEventTypes();
     }
 
 }
