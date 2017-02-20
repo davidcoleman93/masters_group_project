e9ca41322f4243c7525c1ec9048b9bba840da670
D H <D H>
2017-02-20 20:44:14 +0000

    Buiness layer refactored

---
+++ src/main/java/business/FailureEventBusinessImpl.java
@@ -0,0 +1,32 @@
+package business;
+
+import dao.FailureEventDAOLocal;
+
+import javax.annotation.Resource;
+import javax.ejb.Local;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import java.util.Collection;
+
+/**
+ * Created by C06590861 on 16/02/2017.
+ */
+
+@Local
+@Stateless
+public class FailureEventBusinessImpl implements FailureEventBusinessLocal {
+
+    @Inject
+    private FailureEventDAOLocal daoBean;
+
+    @Resource
+    private SessionContext session;
+
+    public Collection<?> getAllFailureEvents(){
+
+        return daoBean.getAllFailureEvents();
+
+    }
+
+}
---
+++ src/main/java/business/UserEventTypeBusinessImpl.java
@@ -0,0 +1,30 @@
+package business;
+
+import dao.UserEventTypeDAOLocal;
+
+import javax.annotation.Resource;
+import javax.ejb.Local;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import java.util.Collection;
+
+/**
+ * Created by david on 19/02/2017.
+ */
+
+@Local
+@Stateless
+public class UserEventTypeBusinessImpl implements UserEventTypeBusinessLocal {
+
+    @Inject
+    private UserEventTypeDAOLocal daoBean;
+
+    @Resource
+    private SessionContext session;
+
+    public Collection<?> getAllUserEventTypes(){
+        return daoBean.getAllUserEventTypes();
+    }
+
+}
