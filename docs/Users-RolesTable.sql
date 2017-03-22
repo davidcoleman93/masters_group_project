b59334a3e97754df8ef618ebc4fe2aa8be2b9ca9
Alexey Sereda <alexey.sereda@mydit.ie>
2017-03-22 17:13:30 +0000

    User-RolesTable updated

--- docs/Users-RolesTable.sql
+++ docs/Users-RolesTable.sql
@@ -4,14 +4,23 @@
 --
 
 DROP TABLE IF EXISTS `lte_failure_system`.`users` ;
+DROP TABLE IF EXISTS `lte_failure_system`.`role_type` ;
+
+--
+-- Table structure for table `role_type`
+--	
+  CREATE TABLE `lte_failure_system`.`role_type` (
+  `role_id` INT NOT NULL,
+  `role_type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`role_id`));
 
 CREATE TABLE `lte_failure_system`.`users` (
-  `id_user` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `role_id` INT NOT NULL,
-  PRIMARY KEY (`id_user`),
-  UNIQUE INDEX `id_user_UNIQUE` (`id_user` ASC),
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC),
   INDEX `role_id_idx` (`role_id` ASC),
   CONSTRAINT `role_id`
@@ -20,16 +29,7 @@ CREATE TABLE `lte_failure_system`.`users` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 	
---
--- Table structure for table `role_type`
---	
-	
-DROP TABLE IF EXISTS `lte_failure_system`.`role_type` ;
 
-  CREATE TABLE `lte_failure_system`.`role_type` (
-  `role_id` INT NOT NULL,
-  `role_type` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`role_id`));
   
   
   
