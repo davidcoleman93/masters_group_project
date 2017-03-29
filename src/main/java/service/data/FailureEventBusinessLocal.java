ddfa1ec4d0f678b442bc61fcb9facad9db7688d8
D H <D H>
2017-03-29 17:39:42 +0100

    Code updates...

--- src/main/java/service/data/FailureEventBusinessLocal.java
+++ src/main/java/service/data/FailureEventBusinessLocal.java
@@ -1,10 +1,6 @@
 package service.data;
 
 import javax.ejb.Local;
-import javax.ejb.TransactionAttribute;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
 
 /**
  * Created by david on 19/02/2017.
