b02bfb8841adba6a18a35ab5eb1c72369bced840
D H <D H>
2017-03-29 17:39:26 +0100

    Code updates...

--- src/main/java/service/data/FailureClassBusinessImpl.java
+++ src/main/java/service/data/FailureClassBusinessImpl.java
@@ -1,40 +1,46 @@
 package service.data;
 
 import dao.FailureClassDAOLocal;
+import dao.UserEventTypeDAOLocal;
 import entities.FailureClass;
+import entities.UserEventType;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 
-import javax.annotation.Resource;
 import javax.ejb.Local;
-import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.inject.Inject;
-import javax.validation.constraints.NotNull;
-import java.io.*;
-import java.util.Collection;
+import java.io.File;
 import java.util.HashSet;
 
 /**
- * Created by C06590861 on 15/02/2017.
+ * Created by david on 28/03/2017.
  */
 
 @Local
 @Stateless
+@TransactionManagement(TransactionManagementType.BEAN)
 public class FailureClassBusinessImpl implements FailureClassBusinessLocal {
 
     @Inject
-    private FailureClassDAOLocal daoBean;
-
-    @Resource
-    private SessionContext session;
+    private FailureClassDAOLocal failureClassBean;
 
     public void updateFailureClasses(String fileName){
         File temp;
         LineIterator lineIterator = null;
 
-        HashSet<Integer> failureMap = null;
+        HashSet<Integer> failureClassSet = null;
         int lineNum = 0;
+
+        //Data types for User Event Types
+        Integer failureClass;
+        String description;
+
+        String[] failureClasses;
+
+        boolean error;
 
         try{
             temp = new File(fileName);
@@ -42,8 +48,9 @@ public class FailureClassBusinessImpl implements FailureClassBusinessLocal {
         }catch(Exception ioe){
         }
 
+        //Data structure to hold the primary keys
         try{
-            failureMap = daoBean.getFailureClassSet();
+            failureClassSet = failureClassBean.getFailureClassSet();
         }catch (Exception e){
             System.out.println("ERROR IN PARSING FAILURE CLASS TO MULTIMAP");
         }
@@ -51,37 +58,41 @@ public class FailureClassBusinessImpl implements FailureClassBusinessLocal {
         try{
             System.out.println("STARTED");
             while(lineIterator.hasNext()){
-                String line = lineIterator.nextLine();
+                failureClasses = lineIterator.nextLine().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                if(lineNum == 0){
+                    lineNum++;
+                }else{
+                    failureClass = null;
+                    description = null;
 
-                if(!line.equals("")){
-                    if(lineNum == 0){
-                        lineNum++;
-                    }else{
-                        lineNum++;
-                        String[] fEvents = line.split(",");
-                        Integer failureClass = -1;
-                        String desc = "";
+                    error = false;
+
+                    try{
+                        failureClass = Integer.parseInt(failureClasses[0]);
+                    }catch (Exception e) {
+                        if(!error) error = true;
+                    }
+                    if(!failureClassSet.contains(failureClass) && !error){
                         try{
-                            failureClass = Integer.parseInt(fEvents[0]);
-                            try{
-                                desc = fEvents[1];
-                            }catch (Exception e){
-                            }
-                        }catch (Exception e) {
+                            description = failureClasses[1];
+                        }catch (Exception e){
+                            if(!error) error = true;
                         }
-                        System.out.println(failureClass + "," + desc);
-                        if(!failureMap.contains(failureClass)){
-                            daoBean.addFailureClass(new FailureClass(failureClass, desc));
+
+                        if(!error){
+                            failureClassBean.addFailureClass(
+                                    new FailureClass(
+                                            failureClass,
+                                            description
+                                    )
+                            );
                         }
                     }
                 }
-
             }
-
         }finally {
             LineIterator.closeQuietly(lineIterator);
         }
         System.out.println("COMPLETED");
     }
-
 }
